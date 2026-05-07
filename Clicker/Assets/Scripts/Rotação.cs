fe210fe1a5e2a8adfd7710d0f0bd92cf8bd7c12f
Otavio-cp <otavio61284596@alunos.sc.senac.br>
2026-05-07 16:21:13 -0300

    Rotação de objeto

---
+++ Clicker/Assets/Scripts/Rotação.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Rotação : MonoBehaviour
+{
+    public float velRotação = 24f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up * velRotação * Time.deltaTime);
+    }
+}
