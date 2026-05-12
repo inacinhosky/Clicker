ec16c5c9ab08fccec94cef7e583a62d369be1698
Otavio-cp <otavio61284596@alunos.sc.senac.br>
2026-05-12 08:49:45 -0300

    Cenario

--- Clicker/Assets/Scripts/Rotação.cs
+++ Clicker/Assets/Scripts/Rotação.cs
@@ -7,7 +7,7 @@ public class Rotação : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Screen.SetResolution(1920, 1080, FullScreenMode.FullScreenWindow);
     }
 
     // Update is called once per frame
